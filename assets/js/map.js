af869fdf6ff154d52108c0bfe86400979baac380
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2026-06-10 09:36:28 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -52,6 +52,15 @@ const redIcon = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
 });
+const yellowIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 L.marker([-65.4918398785205, -46.801231304339915])
   .addTo(map)
