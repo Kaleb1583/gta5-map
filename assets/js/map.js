d496c607b7321b61d4ccc0ae7b003da16f14a60e
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2026-06-10 09:07:41 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -58,6 +58,18 @@ L.marker([-67.27377816220924, -2.3291015625000004])
   .addTo(map)
   .bindPopup("LSPD - Mission Row");
 
+const grayIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.marker([-72.40854450390432, -53.12988281250001], { icon: grayIcon }).addTo(map);
+
 
 map.on('click', function (e) {
     console.log(e.latlng);
