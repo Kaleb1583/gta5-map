3049350708af71713f85d552f6591fac24a223b1
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2026-06-11 03:20:04 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -63,6 +63,53 @@ const yellowIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 
+const lightBlueIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.marker([-78.64483199293961, -40.34020841008464],
+  { icon: redIcon })
+	.addTo(map)
+    .bindPopup("NOOSE - LSIA");
+
+L.marker([-59.104606327504804, 57.14599609375],
+  { icon: redIcon })
+	.addTo(map)
+    .bindPopup("NOOSE HQ - Palmino Highlands");
+
+L.marker([27.823271732708104, -79.46289062500001],
+  { icon: redIcon })
+	.addTo(map)
+    .bindPopup("US Military - Fort Zancudo");
+
+L.marker([12.424287327390914, 34.22851562499999],
+  { icon: redIcon })
+	.addTo(map)
+    .bindPopup("SASPA - Bolingbroke Penitentiary");
+
+
+
+L.marker([-73.1645422233497, -4.855957031250001])
+  .addTo(map)
+  .bindPopup("LSSD Sheriff Station - Davis");
+
+L.marker([40.341094329488506, 38.964843750000014],
+  { icon: yellowIcon })
+	.addTo(map)
+    .bindPopup("LSSD Sheriff Station - Sandy Shores");
+
+L.marker([73.67901687017999, -27.834472656249996],
+  { icon: yellowIcon })
+	.addTo(map)
+    .bindPopup("LSSD Sheriff Station - Paleto Bay");
+
+
+
 L.marker([-65.4918398785205, -46.801231304339915])
   .addTo(map)
   .bindPopup("LSPD - Vespucci");
@@ -79,64 +126,33 @@ L.marker([-70.30150471238284, 9.448242187500002])
   .addTo(map)
   .bindPopup("LSPD - La Mesa");
 
-L.marker([-73.1645422233497, -4.855957031250001])
-  .addTo(map)
-  .bindPopup("LSSD Sheriff Station - Davis");
-
 L.marker([-67.27377816220924, -2.3291015625000004])
   .addTo(map)
   .bindPopup("LSPD - Mission Row");
 
 
 L.marker([-72.40854450390432, -53.12988281250001],
-  { icon: grayIcon })
+  { icon: lightBlueIcon })
 	.addTo(map)
     .bindPopup("LSPD Substation - Vespucci Beach");
 
 L.marker([-67.5902474708725, -62.31201171875],
-  { icon: grayIcon })
+  { icon: lightBlueIcon })
 	.addTo(map)
     .bindPopup("LSPD Substation - Del Perro (Pier)");
+
+
 
 L.marker([-36.78171185627811, -3.8891601562500004],
   { icon: grayIcon })
 	.addTo(map)
-    .bindPopup("SASParkRangers - Beaver Bush (Vinewood Hills)");
+    .bindPopup("SASPR - Beaver Bush (Vinewood Hills)");
 
 L.marker([62.72584244423216, -58.057861328125],
   { icon: grayIcon })
 	.addTo(map)
-    .bindPopup("SASParkRangers - Raton Canyon (Paleto) ");
+    .bindPopup("SASPR - Raton Canyon (Paleto) ");
 
-L.marker([-78.64483199293961, -40.34020841008464],
-  { icon: redIcon })
-	.addTo(map)
-    .bindPopup("NOOSE - LSIA");
-
-L.marker([-59.104606327504804, 57.14599609375],
-  { icon: redIcon })
-	.addTo(map)
-    .bindPopup("NOOSE HQ - Palmino Highlands");
-
-L.marker([40.341094329488506, 38.964843750000014],
-  { icon: yellowIcon })
-	.addTo(map)
-    .bindPopup("LSSD Sheriff Station - Sandy Shores");
-
-L.marker([73.67901687017999, -27.834472656249996],
-  { icon: yellowIcon })
-	.addTo(map)
-    .bindPopup("LSSD Sheriff Station - Paleto Bay");
-
-L.marker([12.424287327390914, 34.22851562499999],
-  { icon: redIcon })
-	.addTo(map)
-    .bindPopup("SASPA - Bolingbroke Penitentiary");
-
-L.marker([27.823271732708104, -79.46289062500001],
-  { icon: redIcon })
-	.addTo(map)
-    .bindPopup("US Military - Fort Zancudo");
 
 map.on('click', function (e) {
     console.log(e.latlng);
